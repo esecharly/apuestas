b73f38964c1e11a06882934ae07071005351558b
esecharly <crls38frack@hotmail.com>
2021-07-15 21:30:48 -0500

    reestructuracion de dependencias en carpeta raw

--- app/dependencias.php
+++ app/dependencias.php
@@ -1,13 +1,13 @@
-<link rel="icon" type="image/ico" href="<?=SERVIDOR;?>img/betking-icon.png">
-<link rel="stylesheet" href="<?=SERVIDOR;?>css/b5/bootstrap.css">
-<link rel="stylesheet" href="<?=SERVIDOR;?>css/sweetalert2/sweetalert2.css">
-<link rel="stylesheet" href="<?=SERVIDOR;?>css/dataTable/dataTables.bootstrap5.min.css">
-<link rel="stylesheet" href="<?=SERVIDOR;?>css/main.css">
+<link rel="icon" type="image/ico" href="<?=SERVIDOR;?>raw/img/betking-icon.png">
+<link rel="stylesheet" href="<?=SERVIDOR;?>raw/css/b5/bootstrap.css">
+<link rel="stylesheet" href="<?=SERVIDOR;?>raw/css/sweetalert2/sweetalert2.css">
+<link rel="stylesheet" href="<?=SERVIDOR;?>raw/css/dataTable/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="<?=SERVIDOR;?>raw/css/main.css">
 
-<script src="<?=SERVIDOR;?>js/b5/bootstrap.bundle.js"></script>
-<script src="<?=SERVIDOR;?>js/font_awesome/all.js"></script>
-<script src="<?=SERVIDOR;?>js/sweetalert2/sweetalert2.js"></script>
-<script src="<?=SERVIDOR;?>js/jquery/jquery-3.6.0.min.js"></script>
-<script src="<?=SERVIDOR;?>js/dataTable/jquery.dataTables.min.js"></script>
-<script src="<?=SERVIDOR;?>js/dataTable/dataTables.bootstrap5.min.js"></script>
+<script src="<?=SERVIDOR;?>raw/js/b5/bootstrap.bundle.js"></script>
+<script src="<?=SERVIDOR;?>raw/js/font_awesome/all.js"></script>
+<script src="<?=SERVIDOR;?>raw/js/sweetalert2/sweetalert2.js"></script>
+<script src="<?=SERVIDOR;?>raw/js/jquery/jquery-3.6.0.min.js"></script>
+<script src="<?=SERVIDOR;?>raw/js/dataTable/jquery.dataTables.min.js"></script>
+<script src="<?=SERVIDOR;?>raw/js/dataTable/dataTables.bootstrap5.min.js"></script>
 
